b49a64e7f66a20f18f75b4abf9674f6b30a659a9
DimaV11 <dmitriy_vostroknutov@mail.ru>
2024-08-04 15:51:08 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=devise-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Веб приложение про IT зависимость!</h1>
+    
+    <p>Это такой вид зависимости, когда человек не может обойтись без использования определёных
+        технологий и устройства.</p>
+
+        <ul>
+            <li>Установить лимиты на время, проведенное в Интернете и на использование устройств.</li>
+            <li>Разнообразить свои интересы и занятия, чтобы не ограничиваться только использованием технологий.</li>
+            <li>Искать новые способы общения с окружающим миром, в том числе с людьми, чтобы уменьшить зависимость от социальных сетей и мессенджеров.</li>
+            <li>Регулярные перерывы и отключение смартфона, чтобы не утомлять глаза и разум.</li>
+            <li>Планирование времени без использования технологий, чтобы расслабиться и отдохнуть.</li>
+            <li>Старайтесь быть более осознанными в отношении использования технологий, и задавайте себе вопросы, когда чувствуете, что становитесь слишком зависимыми.</li>
+        </ul>
+
+        <img src="https://www.bnkomi.ru/content/news/images/59808/iStock_000057190820Large-e1454567260939.jpg" alt="">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/uK-9yRJ9LaI?si=9OEE-YZX5Nuaj-0A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</body>
+</html>
